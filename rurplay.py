0d03ac68074dff4790c374a361d0bafba912ebaa
kenneth2016 <kennethl1801@gmail.com>
2016-08-22 08:48:23 -0600

    progreso en instrucciones

--- rurplay.py
+++ rurplay.py
@@ -1,6 +1,6 @@
-
+mapa_completo =[]
 def cargar_mapa(mapa):
-	mapa_completo =[]
+
 	f = open(mapa , "r")
 
 	for line in f:
@@ -10,3 +10,14 @@ def cargar_mapa(mapa):
 		
 mi_mapa= cargar_mapa("mapas/mapa1.txt")
 print (mapa_completo)
+
+ordenes = []
+def cargar_instrucciones(instrucciones):
+	
+	o = open(instrucciones, "r")
+	for regla in o:
+		ordenes.append(list(regla.split()))
+	return ordenes
+
+reglas= cargar_instrucciones("instrucciones/programa1.txt")
+print (ordenes)
