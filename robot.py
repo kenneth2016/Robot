f8cf9971ef0c24775f3879c0e0afa00611d7f221
kenneth2016 <kennethl1801@gmail.com>
2016-08-23 08:50:21 -0600

    avance

--- robot.py
+++ robot.py
@@ -1,5 +1,31 @@
 class Robot(object):
-	def __init__(self, girar, recoger, caminar):
-		self.girar = girar
-		self.recoger = recoger
-		self.caminar = caminar
+	def __init__(self, direccion, x, y, fichas, mapa):
+		self.direccion = 0
+		self.x = 0
+		self.y = 0
+		self.fichas = 0
+		self.mapa = mapa_completo
+
+	def move(self):
+		if self.direccion==0 and self.x < 80:
+			self.x +=1
+
+		if self.direccion==90 and self.y < 25:
+			self.y +=1
+
+		if self.direccion==180 and self.x < 80:
+			self.x -=1
+
+		if self.direccion==270 and self.y < 25:
+			self.y -=1
+
+	def rotar(self):
+		self.direccion+=90
+		if self.direccion>360:
+			self.direccion=0
+
+	def recoger(self):
+		
+
+
+
