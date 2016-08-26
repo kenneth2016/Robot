5bcbecf4a6c897747dffdc9855ce0dc1fdcc67e6
kenneth2016 <kennethl1801@gmail.com>
2016-08-26 08:47:57 -0600

    WTF

---
+++ mapa.py
@@ -0,0 +1,9 @@
+class Mapa(object):
+	def __init__ (self, ancho, largo):
+		self.ancho=ancho
+		self.alto=alto
+		self.robot=None
+		sel.monedas=[]
+
+	def
+		
--- robot.py
+++ robot.py
@@ -1,11 +1,14 @@
 
 class Robot(object):
 	def __init__(self, direccion, x, y, fichas, mapa):
-		self.direccion = 0
+		self.direccion = 90
 		self.x = 0
 		self.y = 0
 		self.fichas = 0
-		self.mapa = mapa_completo
+		self.mapa = None
+
+	def colocar_en_mapa(self, mapa):
+		self.mapa=mapa
 
 	def move(self):
 		if self.direccion==0 and self.x < 80:
